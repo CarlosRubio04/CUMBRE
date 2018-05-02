c17fa2b07dd69b1bebab5e1a548b7c510893ab37
Carlos Rubio BM <crubio@bmdigital.co>
2018-05-02 12:47:01 -0500

    Estilos Iniciales Formulario de formulación del proyecto

--- views/formulacion.php
+++ views/formulacion.php
@@ -1,14 +1,20 @@
-<section class="main-white">
-	<div class="container">
+<section class="main formulacion">
+	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-12">
-				<h1 class="title">DEFINICIÓN DEL PROYECTO</h1>
-				<hr/>
+			<div class="col-md-10">
+				<h1 class="formulacionTitle">Definición del Proyecto</h1>
+			</div>
+			<div class="col-md-2">
+				<a href="#"><i class="fa fa-angle-double-right fa-2x" aria-hidden="true"></i></a>
 			</div>
 		</div>
 		<div class="row">
 			<form action="">
 				<div class="col-md-6">
+					<div class="form-group">
+						<label for="nombre">Nombre del proyecto</label>
+						<input type="text" name="nombre" class="form-control" placeholder="Resumen">
+					</div>
 					<div class="form-group">
 						<label for="resumen">Resumen</label>
 						<input type="text" name="resumen" class="form-control" placeholder="Resumen">
@@ -17,103 +23,34 @@
 						<label for="introduccion">Introducción</label>
 						<textarea name="intro" id="intro" rows="5" class="form-control" placeholder="Introducción"></textarea>
 					</div>
-					<h4>Problematica</h4>
+					<h4 class="formulacionSubTitle">Problematica</h4>
 					<div class="form-group">
 						<label for="definicion">Definición de problemática</label>
 						<textarea name="definicion" id="" rows="5" class="form-control" placeholder="Definición de problemática"></textarea>
 					</div>
 					<div class="form-group">
 						<label for="">¿Qué Megas de la universidad impacta?</label>
-						<select name="impacto" class="form-control">
-							<option value="">¿Qué Megas de la universidad impacta?</option>
-							<option value="Reconciliación del país">
-								Reconciliación del país
-							</option>
-							<option value="Interculturalidad, internacionalización y cuidado de la casa común">
-								Interculturalidad, internacionalización y cuidado de la casa común
-							</option>
-							<option value="Desarrollo sostenible integral">
-								Desarrollo sostenible integral
-							</option>
-							<option value="Transformar el sistema de toma de decisiones">
-								Transformar el sistema de toma de decisiones
-							</option>
-						</select>
+						<div class="u-selectWraper">
+							<select name="impacto" class="form-control">
+								<option disabled="true">¿Qué Megas de la universidad impacta?</option>
+								<option value="Reconciliación del país">
+									Reconciliación del país
+								</option>
+								<option value="Interculturalidad, internacionalización y cuidado de la casa común">
+									Interculturalidad, internacionalización y cuidado de la casa común
+								</option>
+								<option value="Desarrollo sostenible integral">
+									Desarrollo sostenible integral
+								</option>
+								<option value="Transformar el sistema de toma de decisiones">
+									Transformar el sistema de toma de decisiones
+								</option>
+							</select>
+						</div>
 					</div>
 
 					<div class="form-group">
-						<label for="justificacion">Justificación del proyecto</label>
-						<textarea name="justificacion" id="" rows="5" class="form-control" placeholder="Justificación del proyecto."></textarea>
-					</div>
-					<div class="form-group">
-						<label for="">Descripción del proyecto</label>
-						<textarea name="" id="" rows="5" class="form-control" placeholder="Descripción del proyecto."></textarea>
-					</div>
-					<h4>Objetivos del proyecto</h4>
-					<div class="form-group">
-						<label for="">Objetivo general</label>
-						<textarea name="" id="" rows="5" class="form-control" placeholder="Objetivo general"></textarea>
-					</div>
-					<div class="form-group">
-						<label for="">Objetivos especificos</label>
-						<textarea name="" id="" rows="5" class="form-control" placeholder="Objetivos especificos"></textarea>
-					</div>
-				</div>
-
-				<div class="col-md-6">
-					<h4>Alcance e impacto</h4>
-					<hr>
-					<h4>Tiempo</h4>
-					<hr>
-
-					<div class="form-group">
-						<label for="">Fecha de inicio</label>
-						<input type="date" name="" class="form-control">
-					</div>
-
-					<div class="form-group">
-						<label for="">Fecha de finalización</label>
-						<input type="date" name="" class="form-control">
-					</div>
-					
-					<div class="form-group">
-						<label for="">Metodología</label>
-						<input type="text" name="" class="form-control" placeholder="Metodología">
-					</div>
-					<div class="form-group">
-						<label for="">Resultados esperados</label>
-						<input type="text" name="" class="form-control" placeholder="Resultados esperados">
-					</div>
-					<div class="form-group">
-						<label for="">Cronograma</label>
-						<a href="?content=cronograma" class="btn btn-main">Cronograma</a>
-					</div>
-					<div class="form-group">
-						<label for="">Ejecución del proyecto</label>
-						<input type="text" name="" class="form-control" placeholder="Ejecución del proyecto">
-					</div>
-					<div class="form-group">
-						<label for="">Resultados</label>
-						<input type="text" name="" class="form-control" placeholder="Resultados">
-					</div>
-					<div class="form-group">
-						<label for="">Análisis de resultados</label>
-						<input type="text" name="" class="form-control" placeholder="Análisis de resultados">
-					</div>
-					<div class="form-group">
-						<label for="">Productos</label>
-						<input type="text" name="" class="form-control" placeholder="Productos">
-					</div>
-					<div class="form-group">
-						<label for="">Conclusiones</label>
-						<textarea name="" id="" rows="5" class="form-control" placeholder="Conclusiones"></textarea>
-					</div>
-					<div class="form-group">
-						<label for="">Bibliografía</label>
-						<textarea name="" id="" rows="5" class="form-control" placeholder="Bibliografía"></textarea>
-					</div>
-					<div class="form-group">
-						<button type="submit" class="btn btn-sub">ENVIAR</button>
+						<button class="btn btn-next">SIGUIENTE</button>
 					</div>
 				</div>
 			</form>
